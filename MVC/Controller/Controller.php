cf63d966757d5a4b06f95e80a866c257621e36c5
RobertoHenryck <robertosilva.sesisenai@gmail.com>
2025-02-07 08:24:56 -0300

    Adicionando conteúdo no Model e Controller

---
+++ MVC/Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'C:\Turma2\xampp\htdocs\ROBERTO-ECO-VERDE\MVC\Model\Model.php';
+
+class Controller
+{
+    private $Model;
+
+public function __construct($pdo){
+    $this->Model = new Model($pdo);
+}
+
+public function cadastrarconsumo($consumo_de_estacao,$consumo_do_servidor,$consumo_de_iluminacao,$consumo_de_climatizacao,$consumo_de_equipamentos){
+return $this->Model->cadastrarconsumo($consumo_de_estacao,$consumo_do_servidor,$consumo_de_iluminacao,$consumo_de_climatizacao,$consumo_de_equipamentos);
+}
+
+public function listarconsumo(){
+    return $this->Model->listarconsumo();
+}
+}
---
+++ MVC/Model/Model.php
@@ -0,0 +1,32 @@
+<?php
+class Model
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function cadastrarConsumo($consumo_de_estacao, $consumo_do_servidor, $consumo_de_iluminacao, $consumo_de_climatizacao, $consumo_de_equipamentos)
+    {
+        $sql = "INSERT INTO cadastro_consumo(consumo_de_estacao, consumo_do_servidor, consumo_de_iluminacao, consumo_de_climatizacao, consumo_de_equipamentos) 
+                VALUES(:consumo_de_estacao, :consumo_do_servidor, :consumo_de_iluminacao, :consumo_de_climatizacao, :consumo_de_equipamentos)";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":consumo_de_estacao", $consumo_de_estacao);
+        $stmt->bindParam(":consumo_do_servidor", $consumo_do_servidor);
+        $stmt->bindParam(":consumo_de_iluminacao", $consumo_de_iluminacao);
+        $stmt->bindParam(":consumo_de_climatizacao", $consumo_de_climatizacao);
+        $stmt->bindParam(":consumo_de_equipamentos", $consumo_de_equipamentos);
+
+        return $stmt->execute();
+    }
+
+    public function listarconsumo()
+    {
+        $sql = "SELECT * FROM viagem";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
